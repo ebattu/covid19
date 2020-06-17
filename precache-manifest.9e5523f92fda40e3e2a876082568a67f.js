188794a7a6de58aec1c7f7a0dc93e2a834a69aa1
Eswar <eswardata1@gmail.com>
2020-06-17 17:49:48 -0500

    Updates

---
+++ precache-manifest.9e5523f92fda40e3e2a876082568a67f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bfb9dc69139e5d47fef6a9e3e1fc53a",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "e080b959d7aa0eb763f7",
+    "url": "/covid19/static/css/main.5e9e9c6d.chunk.css"
+  },
+  {
+    "revision": "9ae658167bf98bf6513f",
+    "url": "/covid19/static/js/2.64f23b5e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/covid19/static/js/2.64f23b5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e080b959d7aa0eb763f7",
+    "url": "/covid19/static/js/main.d1405623.chunk.js"
+  },
+  {
+    "revision": "6a3a8572484ce6c489e8",
+    "url": "/covid19/static/js/runtime-main.bbed230b.js"
+  }
+]);
